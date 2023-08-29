9a70286ee19cd1aee2e60b1580705b37e1b5ba5f
VishalCoder78 <vishalcoder78@gmail.com>
2023-08-29 15:53:25 +0530

    video call done

--- server.js
+++ server.js
@@ -19,7 +19,11 @@ io.on('connection', socket => {
     socket.on('join-room', (roomId, userId) =>{
         socket.join(roomId)
         socket.to(roomId).emit('user-connected', userId)
-    } )
+
+        socket.on('disconnect', () => {
+            socket.to(roomId).emit('user-disconnected', userId)
+        })
+    })
 })
 
 server.listen(3000)
